6c480b6fef72179129d28fd4851719ea320fa047
marwenilo <marwenmagrii@gmail.com>
2022-09-26 03:53:21 +0100

    connect the music player when music selected

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,16 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+/* eslint-disable*/
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+const PlayPause = ({ activeSong, isPlaying, song, handlePlay, handlePause }) => (
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle size={35}
+      className='text-gray-300'
+      onClick={handlePause}
+    />
+  ) : (
+    <FaPlayCircle size={35}
+      className='text-gray-300'
+      onClick={handlePlay}
+    />
+  ));
 
 export default PlayPause;
